de4e392707c1d13ff0c70f6465dbd527dd9c26f6
Aliza <erjan984237@gmail.com>
2023-04-04 16:05:37 +0600

    added new nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-export default function App(){
-  return(
+import { NavLink } from "react-router-dom";
+
+export default function App() {
+  return (
     <div className="App">
       <h1> Hello world</h1>
       <p> this my diploma</p>
-      
-      
-      </div>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">About</NavLink>
+      </nav>
+    </div>
   );
-  }
-  
+}
