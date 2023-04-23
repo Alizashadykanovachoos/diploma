682344ae06055c396098646eb460d096eea7e29a
Aliza <erjan984237@gmail.com>
2023-04-23 13:32:02 +0600

    remowe file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,7 @@
 export default function About() {
   return (
     <div className="About">
-      <h1>It is side best in the world</h1>
+      <h1> </h1>
       <p> This is my awesome Laptop shop </p>
     </div>
   );
