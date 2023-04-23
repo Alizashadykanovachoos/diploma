815e81a3a708d810197ee4186b95476b19d5eea6
Aliza <erjan984237@gmail.com>
2023-04-23 20:50:02 +0600

    remove file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,9 @@
-export default function About (){
-  return(
-   <div>fhghkk</div>
+export default  function About(){
+  return (
+    <div className="About">
+      <h1>Welcome to my Laptop shop</h1>
+      <p> Laptop site where you can find any laptop of your choice </p>
+
+    </div>
   );
 }
