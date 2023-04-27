8dc5f7d6ba4c3c4fd783a0d2d7ab90a06e155598
Aliza <erjan984237@gmail.com>
2023-04-27 15:39:35 +0600

    Added new components

---
+++ src/components/OrderList/OrderList.jsx
@@ -0,0 +1,41 @@
+import { useContext } from "react";
+import { AppContext } from "../../App";
+import "./OrderList.css";
+
+export default function OrderList() {
+  const { orders, products } = useContext(AppContext);
+
+  if (!orders.length || !products.length) {
+    return "No orders found.";
+  }
+
+  const output = orders.map(order => {
+    const cart = Object.keys(order.cart).map(productId => {
+      const product = products.find(product => product.id === productId);
+      if (!product) {
+        return "Product not found.";
+      }
+
+      return (
+        <div key={product.id  }>
+          {product.name}: {order.cart[product.id]} X {product.price} $ = {order.cart[product.id] * product.price} $
+        </div>
+      );
+    });
+
+    return (
+      <div key={order.id} className="Order">
+        <div>Name :  {order.name}</div>
+        <div>Phone :  {order.phone}</div>
+        <div>Address :  {order.address}</div>
+        <div>Cart :  {cart}</div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="OrderList">
+      {output}
+    </div>
+  );
+}
