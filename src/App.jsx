122fa1b6f2b50a9f07de24f2c5d62ef4c4813395
Aliza <erjan984237@gmail.com>
2023-05-20 21:25:56 +0600

    remove file

--- src/App.jsx
+++ src/App.jsx
@@ -2,78 +2,118 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Home from "./pages/Home";
 import Category from "./pages/Category";
-import NotFound from "./pages/NotFound";
 import { createContext, useEffect, useState } from "react";
 import { getDocs } from "firebase/firestore";
-import {
-  onAuthChange,
-  onCategoriesLoad,
-  onOrdersLoad,
-  onProductsLoad,
-  ordersCollection,
-  productsCollection,
-} from "./firebase";
-import Product from "./pages/Product";
+import { categoryCollection, onAuthChange, orderCollection, productCollection } from "./firebase";
 import Cart from "./pages/Cart";
+import NotFound from "./pages/NotFound";
+import Product from "./pages/Product";
 import ThankYou from "./pages/ThankYou";
 import Orders from "./pages/Orders";
 
-// Создать контекст, который будет хранить данные.
 export const AppContext = createContext({
   categories: [],
   products: [],
   orders: [],
 
-  // контекст для корзины
-  cart: {}, // содержимое корзинки
-  setCart: () => {}, // изменить содержимое корзики
+  // корзина
+  cart: {},
+  setCart: () => {},
 
-  user: null,
+  user: null, // здесь будет храниться информация про пользователя
 });
 
-function App() {
+export default function App() {
   const [categories, setCategories] = useState([]);
   const [products, setProducts] = useState([]);
   const [orders, setOrders] = useState([]);
 
+  // состояние которое хранит информацию пользователя
+  const [user, setUser] = useState(null);
+
+  // корзина
   const [cart, setCart] = useState(() => {
+    // восстановить содержимое корзинки из памяти браузера.
     return JSON.parse(localStorage.getItem("cart")) || {};
   });
 
-  const [user, setUser] = useState(null);
-
+  // выполнить эту функцию только когда содержимое корзинки меняется
   useEffect(() => {
+    // сохранить содержимое корзинки в памяти браузера
     localStorage.setItem("cart", JSON.stringify(cart));
   }, [cart]);
 
+  // выполнить эту функцию только один раз
   useEffect(() => {
-    onCategoriesLoad(setCategories);
-    onProductsLoad(setProducts);
-    onOrdersLoad(setOrders);
+    // получить категории из списка категорий
+    getDocs(categoryCollection).then((snapshot) => {
+      // категории будут храниться в snapshot.docs
 
-    onAuthChange((user) => {
-      if (user) {
-        user.isAdmin = user.email === "bobbyitkg@gmail.com";
-      }
+      // создать массив для категорий
+      const newCategories = [];
+      // заполнить массив данными из списка категорий
+      snapshot.docs.forEach((doc) => {
+        // doc = категория
+        const category = doc.data();
+        category.id = doc.id;
 
+        newCategories.push(category);
+      });
+      // задать новый массив как состояние комапо
+      setCategories(newCategories);
+    });
+
+    // получить продукты из списка продуктов
+    getDocs(productCollection).then((snapshot) => {
+      // продукты будут храниться в snapshot.docs
+
+      // создать массив для продуктов
+      const newProducts = [];
+      // заполнить массив данными из списка продвук
+      snapshot.docs.forEach((doc) => {
+        // doc = продукт
+        const product = doc.data();
+        product.id = doc.id;
+
+        newProducts.push(product);
+      });
+      // задать новый массив как состояние комапо
+      setProducts(newProducts);
+    });
+
+    // получить продукты из списка продуктов
+    getDocs(orderCollection).then((snapshot) => {
+      // продукты будут храниться в snapshot.docs
+
+      // создать массив для продуктов
+      const newOrders = [];
+      // заполнить массив данными из списка продвук
+      snapshot.docs.forEach((doc) => {
+        // doc = продукт
+        const order = doc.data();
+        order.id = doc.id;
+
+        newOrders.push(order);
+      });
+      // задать новый массив как состояние комапо
+      setOrders(newOrders);
+    });
+
+    onAuthChange(user => {
       setUser(user);
     });
   }, []);
 
   return (
     <div className="App">
-      <AppContext.Provider
-        value={{ categories, products, cart, setCart, user, orders }}
-      >
+      <AppContext.Provider value={{ categories, products, cart, setCart, user, orders }}>
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/about" element={<h2>About</h2>} />
+            <Route path="/category/:path" element={<Category />} />
+            <Route path="/product/:path" element={<Product />} />
             <Route path="/cart" element={<Cart />} />
-            <Route path="/about" element={<h1>About</h1>} />
-            <Route path="/contacts" element={<h1>Contacts</h1>} />
-            <Route path="/delivery" element={<h1>Delivery</h1>} />
-            <Route path="/categories/:slug" element={<Category />} />
-            <Route path="/products/:slug" element={<Product />} />
             <Route path="/thank-you" element={<ThankYou />} />
             <Route path="/orders" element={<Orders />} />
 
@@ -84,5 +124,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
