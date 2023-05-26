7f37e0adb63a102836d267bc6064ca17bdaf03b4
Aliza <erjan984237@gmail.com>
2023-05-26 14:46:28 +0600

    remove file

--- src/pages/Delivery.jsx
+++
@@ -1,9 +0,0 @@
-export default  function Delivery(){
-  return (
-    <div className="Delivery">
-      <h1>Welcome to my Laptop shop</h1>
-      <p> This is my awesome Laptop shop </p>
-
-    </div>
-  );
-}
