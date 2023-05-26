518a735285d67aa5469613759bf87e051d72fcb6
Aliza <erjan984237@gmail.com>
2023-05-26 14:33:04 +0600

    added import,export file

--- src/components/AddProduct/AddProduct.jsx
+++ src/components/AddProduct/AddProduct.jsx
@@ -1,18 +1,61 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AppContext } from "../../App";
+import { productCollection, uploadProductPhoto } from "../../firebase";
+import { addDoc } from "firebase/firestore";
 
-export default function AddProduct() {
+export default function AddProduct({ category }) {
   const { user } = useContext(AppContext);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [picture, setPicture] = useState(null);
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   if (!user || !user.isAdmin) {
     return null;
   }
 
-  function onFormSubmit(event) {
-    const data = new FormData(event.target);
+  function onChangeName(event) {
+    setName(event.target.value);
+  }
+  function onChangePrice(event) {
+    setPrice(event.target.value);
+  }
+  function onChangePicture(event) {
+    const file = event.target.files[0];
+    setPicture(file);
+  }
 
+  function onFormSubmit(event) {
     event.preventDefault();
-    console.log(data.get('picture'));
+
+    if (!picture) {
+      alert("Please upload an image");
+      return;
+    }
+
+    setIsSubmitting(true);
+
+    uploadProductPhoto(picture)
+      .then((pictureUrl) =>
+        addDoc(productCollection, {
+          category: category.id,
+          name: name,
+          price: price,
+          picture: pictureUrl,
+          path: name.replaceAll(" ", "-").toLowerCase(),
+        })
+      )
+      .then(() => {
+        setName("");
+        setPrice("");
+        setPicture(null);
+      })
+      .catch((error) => {
+        console.log("Failed to add product:", error);
+      })
+      .finally(() => {
+        setIsSubmitting(false);
+      });
   }
 
   return (
@@ -20,15 +63,38 @@ export default function AddProduct() {
       <form onSubmit={onFormSubmit}>
         <h3>Create a new product</h3>
         <label>
-          Name: <input type="text" name="name" required />
+          Name:
+          <input
+            type="text"
+            value={name}
+            name="name"
+            onChange={onChangeName}
+            required
+          />
         </label>
         <label>
-          Price: <input type="number" name="price" min={0} required /> 
+          Price:
+          <input
+            type="number"
+            value={price}
+            name="price"
+            onChange={onChangePrice}
+            min={0}
+            required
+          />
         </label>
         <label>
-          Picture: <input type="file" name="picture" required />
+          Picture:
+          <input
+            type="file"
+            name="picture"
+            onChange={onChangePicture}
+            required
+          />
         </label>
-        <button>Submit</button>
+        <button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? "Submitting..." : "Submit"}
+        </button>
       </form>
     </div>
   );
