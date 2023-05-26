a1de272f87876180d659403a1db3b278d6073d6f
Aliza <erjan984237@gmail.com>
2023-05-26 14:45:18 +0600

    remowe file

--- src/pages/About.jsx
+++
@@ -1,9 +0,0 @@
-export default  function About(){
-  return (
-    <div className="About">
-      <h1>Welcome to my Laptop shop</h1>
-      <p> Laptop site where you can find any laptop of your choice </p>
-
-    </div>
-  );
-}
