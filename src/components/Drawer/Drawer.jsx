45c8fe8f71ea33f2d72fd60d95a517aed64ad8b1
Aliza <erjan984237@gmail.com>
2023-05-26 14:38:26 +0600

    added Drawer file

--- src/components/Drawer/Drawer.jsx
+++ src/components/Drawer/Drawer.jsx
@@ -4,14 +4,14 @@ import "./Drawer.css"
 
 function Drawer({ open, toggle }) {
   const drawerClassNames = `Drawer ${open ? "open" : ""}`;
-
+  
   return (
-    <div className={drawerClassNames}>
+    <div className={drawerClassNames}> 
       <div onClick={toggle} className="backdrop"></div>
       <div className="content">
         <Logo />
         <Nav />
-      </div>
+      </div>  
     </div>
   );
 }
