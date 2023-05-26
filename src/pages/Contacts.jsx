5dec93e0dbd7d19928982db29d3e570fd0652197
Aliza <erjan984237@gmail.com>
2023-05-26 14:46:09 +0600

    remove file

--- src/pages/Contacts.jsx
+++
@@ -1,9 +0,0 @@
-export default  function Contacts(){
-  return (
-    <div className="Contacts">
-      <h1>Welcome to my Laptop shop</h1>
-      <p> This is my awesome Laptop shop </p>
-
-    </div>
-  );
-}
