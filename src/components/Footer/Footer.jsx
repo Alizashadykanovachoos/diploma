284bf970fea3cb6c9a6eaaaa0aeff40bdde91f65
Aliza <erjan984237@gmail.com>
2023-05-27 15:37:23 +0600

    remowe file

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="Footer">
+      <h1>Footer</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, expedita. Id at illo, temporibus ullam expedita voluptas! Ipsa placeat rem veritatis quos culpa blanditiis amet quo neque necessitatibus exercitationem sunt consectetur excepturi, consequatur laboriosam provident? Culpa nesciunt et officiis aperiam.</p>
+    </footer>
+  );
+}
