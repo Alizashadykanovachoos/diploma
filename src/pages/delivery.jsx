185a679b7ae621936b87fe800c30bf9794aeabaa
Aliza <erjan984237@gmail.com>
2023-05-27 15:39:57 +0600

    remowe file

---
+++ src/pages/delivery.jsx
@@ -0,0 +1,8 @@
+export default function delivery() {
+  return (
+    <div className="delivery">
+      <h1>delivery</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, expedita. Id at illo, temporibus ullam expedita voluptas! Ipsa placeat rem veritatis quos culpa blanditiis amet quo neque necessitatibus exercitationem sunt consectetur excepturi, consequatur laboriosam provident? Culpa nesciunt et officiis aperiam.</p>
+    </div>
+  );
+}
