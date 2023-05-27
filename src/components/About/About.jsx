f6484a3f5fe616ff65aba965deccddc95a37af24
Aliza <erjan984237@gmail.com>
2023-05-27 15:36:41 +0600

    added new file

---
+++ src/components/About/About.jsx
@@ -0,0 +1,9 @@
+export default function About() {
+  return (
+    <div className="About">
+      <h1>Welcome to my Laptop shop </h1>
+      <p>This is my awesome Laptop shop.</p>
+      <p>laptop site where you can find any laptop of your choice</p>
+    </div>
+  );
+}
