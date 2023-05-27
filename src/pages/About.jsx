693ec0c934cb3d20bc1d802c4933b653c155e21f
Aliza <erjan984237@gmail.com>
2023-05-27 15:39:08 +0600

    added export default file

---
+++ src/pages/About.jsx
@@ -0,0 +1,13 @@
+export default function About() {
+  return (
+    <div className="About">
+      <h1></h1>
+      <p>The online store Laptop-Shop.kg began its work in 2023.
+From the very beginning, the main goal was to offer our customers the widest range of notebooks.
+
+We currently offer a full range of laptops
+  
+ Our work is based on qualified assistance in choosing equipment, polite communication with customers and timely delivery.</p>
+    </div>
+  );
+}
