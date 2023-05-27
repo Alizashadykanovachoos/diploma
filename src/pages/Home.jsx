c3ce5cbcb931b3768302b68ffbe5b979e9b7b0e2
Aliza <erjan984237@gmail.com>
2023-05-27 16:40:29 +0600

    added new file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,12 @@ export default function Home() {
       <h1>Welcome to my Laptop shop </h1>
       <p>This is my awesome Laptop shop.</p>
       <p>laptop site where you can find any laptop of your choice</p>
-    </div>
-  );
-}
+      <div className="change-photo">
+            <img
+              src="https://www.youtube.com/watch?v=eDnCTUABK5A&ab_channel=Acer"
+              alt="https://www.youtube.com/watch?v=eDnCTUABK5A&ab_channel=Acer"
+            />
+          </div>
+        </div> 
+  )
+};
