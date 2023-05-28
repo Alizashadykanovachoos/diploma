75ab81eee613b776dac30095afbe462b577509d8
Aliza <erjan984237@gmail.com>
2023-05-28 12:02:05 +0600

    added div file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,103 @@
+
 export default function About() {
   return (
     <div className="About">
-      <h1></h1>
-      <p>The online store Laptop-Shop.kg began its work in 2023.
+      
+      <div >
+          <div >
+            <h1>Welcome to my Laptop Shop</h1>
+            <h3 className="container">  The online store Laptop-Shop.kg began its work in 2023.
 From the very beginning, the main goal was to offer our customers the widest range of notebooks.
 
-We currently offer a full range of laptops
-  
- Our work is based on qualified assistance in choosing equipment, polite communication with customers and timely delivery.</p>
-    </div>
+Currently, we offer the entire range of computer hardware and software both in stock and on order.
+
+Our company has been successfully operating in the computer equipment market for several years and is proud of the trust of our regular customers. Our work is based on qualified assistance in choosing equipment, polite communication with customers and timely delivery.</h3>
+          </div>
+          <div className="home-photo-categorii">
+        
+       
+        <div className="change-photos">
+          <div class="change-photo">
+            <img
+              src=""
+              alt=""
+            />
+          </div>
+          <div className="change-photo">
+            <img
+              src="  "
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="change-photos">
+          <div class="change-photo">
+            <img
+              src="https://www.buildpriceoption.com/wp-content/uploads/2023/03/emergency-assistance-kit.jpg"
+              alt=""
+            />
+          </div>
+          <div className="change-photo">
+            <img
+              src="https://cdn11.bigcommerce.com/s-ymkh4bv/images/stencil/500x659/products/3963/17974/LC09303103_1-WEB__23656.1620952096.jpg?c=2"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="change-photos">
+          <div class="change-photo">
+            <img
+              src="https://i.pinimg.com/736x/b2/7e/fa/b27efa17e17cfc265dfb69c3fe14cc6f.jpg"
+              alt=""
+            />
+          </div>
+          <div className="change-photo">
+            <img
+              src="https://cdn.shopify.com/s/files/1/0032/3921/5222/products/il_fullxfull.3009720120_h92v_1024x1024.jpg?v=1618256267"
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="change-photos">
+          <div class="change-photo">
+            <img
+              src="https://www.longolexus.com/static/dealer-17473/Genuine-Lexus-Parts-2.jpg"
+              alt=""
+            />
+          </div>
+          <div className="change-photo">
+            <img
+              src="https://prod-web-cms-images.s3-us-west-2.amazonaws.com/ce70861e-6367-4e98-b0b7-9739f833f71b/20210416-ORLPM-genuine_parts.jpg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+     
+        </div>
+        <div className="about-text container">
+            <h1>Electric Future</h1><h3>
+            The engine of the future will be powered by electricity. And the
+            future starts here and now. We invite you to take a glimpse at the
+            innovations that will define our mobility. In doing so, we span the
+            spectrum from today’s technologies to hydrogen and upcoming
+            technologies. This electric future offers tremendous opportunities
+            for a greener and more sustainable society.
+          </h3>
+          </div>
+          <div className="about-el">
+            <div className="electric-img"><img src="https://www.bmw.com/content/dam/bmw/marketBMWCOM/bmw_com/categories/sustainability/i4-norway/i4n-08-media-hd.jpg?imwidth=1280" alt="" /></div>
+            <div className="electric-img"><img src="https://www.bmw.com/content/dam/bmw/marketBMWCOM/bmw_com/events/ces2022/ixm60/BMW_CES_07_ixm60_hd.jpg?imwidth=1280" alt="" /></div>
+            <div className="electric-img"><img src="https://www.bmw.com/content/dam/bmw/marketBMWCOM/bmw_com/events/iaa2021/overview/Events_teaser_hd.png?imwidth=1280" alt="" /></div>
+          </div>
+
+
+
+          
+        </div>
+      
+
+     
   );
 }
+
