3b70996033344beea06d92351db74499b5bcf7be
Aliza <erjan984237@gmail.com>
2023-05-28 21:34:47 +0600

    added video file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,21 @@
 export default function Home() {
   return (
-    <div className="Home">
+    <div className="Home"> 
       <h1>Welcome to my Laptop shop </h1>
       <p>This is my awesome Laptop shop.</p>
       <p>laptop site where you can find any laptop of your choice</p>
-      <video autoPlay loop>
-        < source src="https://www.youtube.com/watch?v=eDnCTUABK5A&feature=youtu.be" />
-      </video>
+      <video src="https://www.youtube.com/watch?v=TM0C0eZbJIY&ab_channel=ASUSCIS" controls></video>
+
+     
+  
+  
 
 
 
       </div>
 
   );
-};
+}; 
             
 
 
