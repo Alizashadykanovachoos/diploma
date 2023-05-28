7776e7558c3572adc85dc22e17c9dae8a4a3c2aa
Aliza <erjan984237@gmail.com>
2023-05-28 12:05:36 +0600

    added new file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,12 +4,17 @@ export default function Home() {
       <h1>Welcome to my Laptop shop </h1>
       <p>This is my awesome Laptop shop.</p>
       <p>laptop site where you can find any laptop of your choice</p>
-      <div className="change-photo">
-            <img
-              src="https://www.youtube.com/watch?v=eDnCTUABK5A&ab_channel=Acer"
-              alt="https://www.youtube.com/watch?v=eDnCTUABK5A&ab_channel=Acer"
-            />
-          </div>
-        </div> 
-  )
+      <video autoPlay loop>
+        < source src="https://www.youtube.com/watch?v=eDnCTUABK5A&feature=youtu.be" />
+      </video>
+
+
+
+      </div>
+
+  );
 };
+            
+
+
+  
