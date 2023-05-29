a4a2a25ed7136b1cc9b75e32df606ff6e4872ecb
Aliza <erjan984237@gmail.com>
2023-05-29 21:26:58 +0600

    added new file

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,52 +1,41 @@
-
 export default function About() {
   return (
     <div className="About">
-      
-      <div >
-          <div >
-          <div className="glava"> <h1>Welcome to my Laptop Shop !</h1> </div>
-            <h3 className="container"> 
-            
-            
-            
-             The online store Laptop-Shop.kg began its work in 2023.
-From the very beginning, the main goal was to offer our customers the widest range of notebooks. 
-
- <h3>Currently, we offer the entire range of computer hardware and software both in stock and on order. </h3>
-
-Our company has been successfully operating in the computer equipment market for several years and is proud of the trust of our regular customers. Our work is based on qualified assistance in choosing equipment, polite communication with customers and timely delivery. </h3>
- 
-
-
+      <div>
+        <div>
+          <div className="glava">
+            {" "}
+            <h1>Welcome to my Laptop Shop !</h1>{" "}
           </div>
-          <div className="home-photo-categorii">
-        
-       
-        <div className="photo"> 
-          <div class="photo">
-            <img
-              src="https://phonoteka.org/uploads/posts/2022-02/thumbs/1644246898_2-phonoteka-org-p-polnii-chernii-fon-2.jpg"
-              alt=""
-            />
+          <h3 className="container">
+            The online store Laptop-Shop.kg began its work in 2023. From the
+            very beginning, the main goal was to offer our customers the widest
+            range of notebooks.
+            <h3>
+              Currently, we offer the entire range of computer hardware and
+              software both in stock and on order.{" "}
+            </h3>
+            Our company has been successfully operating in the computer
+            equipment market for several years and is proud of the trust of our
+            regular customers. Our work is based on qualified assistance in
+            choosing equipment, polite communication with customers and timely
+            delivery.{" "}
+          </h3>
+        </div>
+        <div className="home-photo-categorii">
+          <div className="photo">
+            <div class="photo">
+              <img
+                src="https://phonoteka.org/uploads/posts/2022-02/thumbs/1644246898_2-phonoteka-org-p-polnii-chernii-fon-2.jpg"
+                alt=""
+              />
+            </div>
+            <div className="change-photo">
+              <img src="" alt="" />
+            </div>
           </div>
-          <div className="change-photo">
-            <img
-              src=""
-              alt=""
-            />
-          </div>
-          </div>
-          </div>
-          </div>
-          </div>
-          );
-          };
-          
-          
-        
-
-
-
-          
-
+        </div>
+      </div>
+    </div>
+  );
+}
