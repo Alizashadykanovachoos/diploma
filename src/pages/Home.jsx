36a3fa50844044dcf96ba7fb0b8355db98fc71bd
Aliza <erjan984237@gmail.com>
2023-05-29 21:27:26 +0600

    added new file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,22 +1,14 @@
 export default function Home() {
   return (
-    <div className="Home"> 
+    <div className="Home">
       <h1>Welcome to my Laptop shop </h1>
-      <p>This is my awesome Laptop shop.</p>
-      <p>laptop site where you can find any laptop of your choice</p>
-      <video src="https://www.youtube.com/watch?v=TM0C0eZbJIY&ab_channel=ASUSCIS" controls></video>
-
-     
-  
-  
-
-
-
-      </div>
-
+      
+        <p>This is my awesome Laptop shop.</p>
+        <p>laptop site where you can find any laptop of your choice</p>
+        
+        
+        </div>
+      
+    
   );
-}; 
-            
-
-
-  
+}
