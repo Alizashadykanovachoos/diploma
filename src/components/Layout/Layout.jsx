6b2825a2df49724c6a1cbaeb013d49ff8d6b99c4
Aliza <erjan984237@gmail.com>
2023-05-29 12:58:16 +0600

    test

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import Auth from "../Auth/Auth";
-import CartLink from "../CartLink/CartLink";
 import CategoryList from "../CategoryList/CategoryList";
 import Logo from "../Logo/Logo";
 import Nav from "../Nav/Nav";
@@ -8,6 +7,7 @@ import "./Layout.css";
 import NavToggle from "../NavToggle/NavToggle";
 import Drawer from "../Drawer/Drawer";
 import Footer from "../Footer/Footer";
+import CartLink from "../CartLink/CartLink";
 
 export default function Layout(props) {
   const [drawerOpen, setDrawerOpen] = useState(false);
@@ -22,8 +22,8 @@ export default function Layout(props) {
         <Nav />
         <NavToggle callback={toggleDrawer} />
         <Drawer open={drawerOpen} toggle={toggleDrawer} />
-          <CartLink />
-          <Auth />
+        <CartLink />
+        <Auth />
       </header>
       <aside>
         <CategoryList />
