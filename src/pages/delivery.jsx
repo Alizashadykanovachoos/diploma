8adc33c5348aee84de123a77a45b70c0086329c8
Aliza <erjan984237@gmail.com>
2023-05-29 12:55:14 +0600

    corrected Delivary page

--- src/pages/delivery.jsx
+++
@@ -1,8 +0,0 @@
-export default function delivery() {
-  return (
-    <div className="delivery">
-      <h1>delivery</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis, expedita. Id at illo, temporibus ullam expedita voluptas! Ipsa placeat rem veritatis quos culpa blanditiis amet quo neque necessitatibus exercitationem sunt consectetur excepturi, consequatur laboriosam provident? Culpa nesciunt et officiis aperiam.</p>
-    </div>
-  );
-}
