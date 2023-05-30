5af9fa427e67dbb1dc54ad8b4cee3b1f74631ffc
Aliza <erjan984237@gmail.com>
2023-05-30 06:54:35 +0600

    added div file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,9 @@ export default function Home() {
   return (
     <div className="Home">
       <h1>Welcome to my Laptop shop </h1>
-      
-        <p>This is my awesome Laptop shop.</p>
-        <p>laptop site where you can find any laptop of your choice</p>
-        
-        
-        </div>
-      
-    
+
+      <p>This is my awesome Laptop shop.</p>
+      <p>laptop site where you can find any laptop of your choice</p>
+    </div>
   );
 }
